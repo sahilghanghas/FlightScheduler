e83cdd9aae27273e01edfa6707bdd79f6aa64ec5
sahil <sahilghanghas93@gmail.com>
2019-05-06 00:58:20 -0400

    utc tests added

---
+++ UTC1Tests.java
@@ -0,0 +1,26 @@
+package set09;
+
+
+// test's the UTC1 file
+public class UTC1Tests {
+	
+	public static void main (String[] args) {
+        
+		// test objects
+		UTC t1 = new UTC1 (15, 31);
+        UTC t2 = new UTC1 (14, 31);
+        UTC t3 = new UTC1 (15, 32);
+        UTC t4 = new UTC1 (15, 31);
+        
+        // tests
+        assert t1.hour() == 15 : "wrong hour for t1";
+        assert t1.minute() == 31 : "wrong minute for t1";
+
+        assert t1.isEqual (t1) : "isEqual says this doesn't equal this";
+        assert t1.isEqual (t4) : "isEqual says this doesn't equal that";
+        assert ! (t1.isEqual (t2)) : "isEqual true but hour different";
+        assert ! (t1.isEqual (t3)) : "isEqual true but minute different";
+
+        System.out.println ("All unit tests of UTC1 passed.");
+    }
+}
---
+++ UTCsTests.java
@@ -0,0 +1,32 @@
+package set09;
+
+// test's the UTCs file
+public class UTCsTests {
+	public static void main (String[] args) {
+
+        // We'll do these tests several times, to increase the
+        // probability that objects of different classes will be created.
+
+        int NTESTS = 5;    // how many times we'll run each test
+
+        for (int i = 0; i < NTESTS; i = i + 1) {
+            
+        	// test objects
+        	UTC t0000 = UTCs.make (0, 0);      // always test boundary cases
+            UTC t0059 = UTCs.make (0, 59);
+            UTC t2300 = UTCs.make (23, 0);
+            
+            // tests
+            assert t0000.hour() == 0    : "wrong hour for t0000";
+            assert t0000.minute() == 0  : "wrong minute for t0000";
+            assert t0059.hour() == 0    : "wrong hour for t0059";
+            assert t0059.minute() == 59 : "wrong minute for t0059";
+            assert t2300.hour() == 23 : "wrong minute for t2300";
+            assert t2300.minute() == 0 : "wrong minute for t2300";
+            
+        }
+		
+        
+		System.out.println ("UTCs tests passed.");
+    }
+}
